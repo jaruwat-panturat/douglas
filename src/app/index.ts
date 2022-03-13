f84e7b63a49d6a1a2626a3c8989ece491155102c
jaruwat-panturat <panturah@gmail.com>
2022-03-13 16:27:09 +0700

    LineService added

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import { LineService } from './services/Line.service'
 
 const app = express();
 const port = 3000;
+const lineService = new LineService();
 
 app.get('/', (req, res) => {
     res.send('Douglas!!!!!!!');
+    lineService.sendPush('test');
 });
 
 app.listen(port, () => {
