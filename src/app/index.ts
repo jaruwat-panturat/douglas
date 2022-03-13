2089bbd99f12ec69ed8b33ed0799ebf0d177e656
jaruwat-panturat <panturah@gmail.com>
2022-03-13 14:45:12 +0700

    typeScript ingetrated

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,12 @@
-"use strict";
-const express = require('express');
+import express from 'express';
+
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
     res.send('Douglas!!!!!!!');
 });
+
 app.listen(port, () => {
     console.log(`Douglas app listening on port ${port}!`);
 });
