73dc4100fdb1f689b14409afd574d3b4da2b07d4
jaruwat-panturat <panturah@gmail.com>
2022-03-13 20:28:29 +0700

    eslint config updated

--- src/app/services/Line.service.ts
+++ src/app/services/Line.service.ts
@@ -2,24 +2,24 @@ import credentials from '../../../assets/credentials.json';
 import axios from 'axios';
 
 export class LineService {
-    lineUrl = 'https://notify-api.line.me/api/notify';
-    lineHeader = {
-        'Authorization': 'Bearer '+credentials.line.lineToken,
-        'Content-Type': 'application/x-www-form-urlencoded',
-    };
+  lineUrl = 'https://notify-api.line.me/api/notify';
+  lineHeader = {
+    'Authorization': 'Bearer '+credentials.line.lineToken,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
 
-    sendPush(msg: string) {
-        const params = new URLSearchParams();
-        params.append('message', msg);
+  sendPush(msg: string) {
+    const params = new URLSearchParams();
+    params.append('message', msg);
 
-        axios.post(this.lineUrl, params,
-            {
-            headers: this.lineHeader,
-            }).then(function(response) {
-        console.log(response);
-        }).catch(function(error) {
-        console.log(error);
-        }).then(function() {
-        });
-    }
+    axios.post(this.lineUrl, params,
+        {
+          headers: this.lineHeader,
+        }).then(function(response) {
+      console.log(response);
+    }).catch(function(error) {
+      console.log(error);
+    }).then(function() {
+    });
+  }
 }
