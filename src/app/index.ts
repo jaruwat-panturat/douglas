f6cd61eeeca8ccfedc3dfa378844530dab3aa5e3
jaruwat-panturat <panturah@gmail.com>
2022-03-23 23:18:56 +0700

    test github hook

--- src/app/index.ts
+++ src/app/index.ts
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Douglas app listening on port ${port}!`);
+  console.log(`Douglasss app listening on port ${port}!`);
 });
